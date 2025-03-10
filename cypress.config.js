8ea9487e0971dd427c8138f40eeb2bd18fd8cd86
Beatriz Maia <beatriz.alves.maia@gmail.com>
2025-03-10 10:45:55 -0300

    fixing projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectID: 'pqxwxe',
+  projectId: 'pqxwxe',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {}  
